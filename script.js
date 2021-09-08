1041fa3d61cb078b243135719aac7c2bda4632ab
Dave <david.mathaga@student.moringaschool.com>
2021-09-08 17:43:27 +0300

    Writing formula to get the day of the week

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const daysOfTheWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 const maleNames = ["kwasi", "Kwandwo", "Kwabena", "kwaku", "yaw", "kofi", "kwame"];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function birthIndex(d, m, yr) {
+    let dd = parseInt(d);
+    let mm = parseInt(m);
+    let cc = Math.floor(yr / 100);
+    let yy = parseInt(yr.slice(2, 4));
+    let dayOfTheWeek = Math.floor((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7);
+    return dayOfTheWeek;
+}
+
