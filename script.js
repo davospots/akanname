0b01efc3479e4afdc0dbb9d914bf351750772fa3
Dave <david.mathaga@student.moringaschool.com>
2021-09-08 17:26:36 +0300

    Writing arrays of the week ,male and female names

---
+++ script.js
@@ -0,0 +1,3 @@
+const daysOfTheWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const maleNames = ["kwasi", "Kwandwo", "Kwabena", "kwaku", "yaw", "kofi", "kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
