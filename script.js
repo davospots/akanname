8754df9b946c8888103bfa74f8a9f03a11be993c
Dave <david.mathaga@student.moringaschool.com>
2021-09-08 18:25:54 +0300

    making condition statements for checking the input

--- script.js
+++ script.js
@@ -21,14 +21,70 @@ akanForm.addEventListener("submit", (e) => {
     maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 
-    femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    
+
     if (dayOfTheWeek == 0 && gender == "female") {
         
         akanName = femaleAkanNames[0];
-
     } else if (dayOfTheWeek == 0 && gender == "male") {
-
+        
         akanName = maleAkanNames[0];
 
-}
+
+    } else if (dayOfTheWeek == 1 && gender == "female") {
+        
+        akanName = femaleAkanNames[1];
+    } else if (dayOfTheWeek == 1 && gender == "male") {
+        
+        akanName = maleAkanNames[1];
+
+
+    } else if (dayOfTheWeek == 2 && gender == "female") {
+        
+        akanName = femaleAkanNames[2];
+    } else if (dayOfTheWeek == 2 && gender == "male") {
+        
+        akanName = maleAkanNames[2];
+
+
+    } else if (dayOfTheWeek == 3 && gender == "female") {
+        
+        akanName = femaleAkanNames[3];
+    } else if (dayOfTheWeek == 3 && gender == "male") {
+        
+        akanName = maleAkanNames[3];
+
+
+    } else if (dayOfTheWeek == 4 && gender == "female") {
+        
+        akanName = femaleAkanNames[4];
+    } else if (dayOfTheWeek == 4 && gender == "male") {
+        
+        akanName = maleAkanNames[4];
+
+
+    } else if (dayOfTheWeek == 5 && gender == "female") {
+        
+        akanName = femaleAkanNames[5];
+    } else if (dayOfTheWeek == 5 && gender == "male") {
+        
+        akanName = maleAkanNames[5];
+
+
+    } else if (dayOfTheWeek == 6 && gender == "female") {
+        
+        akanName = femaleAkanNames[6];
+    } else if (dayOfTheWeek == 6 && gender == "male") {
+        
+        akanName = maleAkanNames[6];
+    }
+
+
+    let result = document.getElementById("result").innerHTML = "Drumrolls!!! <br>You were born on a " + bornDay + " your Akan name is " + akanName + "!"
+    document.getElementById("akanForm").reset();
+});
+    
+
+
+
+
+
